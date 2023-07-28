e65718f71c3d4468a9d5ec46602dfd16c1615986
Amjad-Khan-360 <amjadkhanniazi010@gmail.com>
2023-07-29 02:40:52 +0500

    After adding jwt packages and adding configuration code to program.cs file

---
+++ SP3/JwtHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SP3
+{
+    public static class JwtHelper
+    {
+        public static string GenerateJwtToken(string userId, string secretKey)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, userId)
+                    // You can add more claims here if needed, e.g., roles, custom claims, etc.
+                }),
+                Expires = DateTime.UtcNow.AddHours(1), // Token expiration time
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
